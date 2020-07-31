b0e0c58cd409e2e3a6c7fda9093500e3405b7749
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-07-30 22:08:05 -0500

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Laptop Customizer
-_Starter project for React course_
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ precache-manifest.4022fa8bc533d38352f6eda28a3941e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a147ae9d5ac327f6962d645873088a00",
+    "url": "/react-laptop-customizer/index.html"
+  },
+  {
+    "revision": "ce6acaad754b919bb3ec",
+    "url": "/react-laptop-customizer/static/css/main.7806e47b.chunk.css"
+  },
+  {
+    "revision": "8123c344f20b6d30d7b3",
+    "url": "/react-laptop-customizer/static/js/2.8bd02bd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-laptop-customizer/static/js/2.8bd02bd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce6acaad754b919bb3ec",
+    "url": "/react-laptop-customizer/static/js/main.6a9f16c7.chunk.js"
+  },
+  {
+    "revision": "4e218b5e9daf0e7d6342",
+    "url": "/react-laptop-customizer/static/js/runtime-main.6548c473.js"
+  }
+]);
